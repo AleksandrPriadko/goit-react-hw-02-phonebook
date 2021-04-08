c9558e93e5f0abda1966d56b204d0a40da56e0fb
AleksandrPriadko <68820567+AleksandrPriadko@users.noreply.github.com>
2021-04-08 23:35:32 +0300

    hz

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from "react";
 import Phonebook from "./components/PhoneBook";
 
 class App extends Component {
+  formSubmitHandler = (data) => {
+    console.log(data);
+  };
   render() {
-    return <Phonebook />;
+    return <Phonebook onSubmit={this.formSubmitHandler} />;
   }
 }
 
--- src/components/PhoneBook.jsx
+++ src/components/PhoneBook.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 class Phonebook extends Component {
   state = {
@@ -6,12 +7,31 @@ class Phonebook extends Component {
     name: "",
   };
 
+  handleChange = (e) => {
+    //console.log(e.currentTarget);
+    const { name, value } = e.currentTarget;
+    this.setState({
+      contacts: [
+        {
+          id: uuidv4(),
+          name: value,
+        },
+      ],
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.contacts);
+  };
+
   render() {
     return (
       <div>
         <h1>Phonebook</h1>
 
-        <form action="">
+        <form action="" onSubmit={this.handleSubmit}>
           <label htmlFor="">
             Name
             <input
@@ -19,11 +39,16 @@ class Phonebook extends Component {
               name="name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              onChange={this.handleChange}
               required
             />
           </label>
           <button type="submite">Add contact</button>
         </form>
+        <h2>Contacts</h2>
+        <ul>
+          <li></li>
+        </ul>
       </div>
     );
   }
